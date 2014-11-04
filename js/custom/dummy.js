8418015de901e714c5c39842acf2002e41ea9a73
tim-peters <tim_peters@gmx.de>
2014-11-04 16:51:58 +0100

    clubs & user aus JSON (angular integration)

--- js/custom/dummy.js
+++ js/custom/dummy.js
@@ -4,14 +4,38 @@ var values = [];
 
 $(document).on('click', '#main li', function(event) {
 	event.preventDefault();
-	values = [];
+	
+	name = $(this).find("h4").text();
+	id = $(this).attr("id");
+	values = {"name":name,"_id":id,"attr":[]};
+	
 	$(this).find('div').each(function(){
 		var heightInPx = $(this).css('height');
 		var height = parseInt(heightInPx.replace('px','')) * parseInt($('.weights input[type="radio"]:checked').val());
-		values.push(height);
+		values.attr.push(height);
 	})
 	// Das ist nur um die werte mal in der Console anzeigen zu lassen
 	.promise().done(function(){
 		console.log(values);
 	});		
 });
+
+// Angular Database Requests
+'use strict';
+
+angular.module('dummy', [])
+	// Holt die Clubs (samt Eigenschaften) aus einem json und speichert sie für angular lesbar 
+  .controller('ClubsCtrl', function($scope, $http){
+    $http.get('./db/clubs.json').then(function(clubsResponse) {
+      $scope.clubs = clubsResponse.data;
+    });
+  })
+  
+  // Holt die Benutzer (samt Eigenschaften) aus einem json und speichert sie für angular lesbar 
+  .controller('UsersCtrl', function($scope, $http){
+    $http.get('./db/users.json').then(function(usersResponse) {
+      $scope.users = usersResponse.data;
+    });
+  });
+  
+  // Die beiden JSONs sollten dann später (serverseitig) aus der DB erzeugt werden
